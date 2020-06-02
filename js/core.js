4a1369f4b194c7bdb7658f9ff1bdda923b3b6c78
DT <david.d.tran@gmail.com>
2020-06-02 00:52:24 -0400

    ADD SCROLLING

--- js/core.js
+++ js/core.js
@@ -1,3 +1,25 @@
-$(document).ready( ()=> {
-	console.log('app loaded');
-});
+
+
+window.onload = (event) => {
+
+	new fullpage('#fullpage', {
+		//options here
+		autoScrolling:true,
+		scrollHorizontally: true,
+		onLeave: function( origin, destination, direction){
+
+
+			console.info(origin.index, destination.index);
+			if (origin.index >= 0) {
+
+				$('body')
+					.removeClass(`v${origin.index}`)
+					.addClass(`v${destination.index}`);
+				
+			}
+		}
+	});
+
+	fullpage_api.setAllowScrolling(true);
+};
+
